0481f151ed1a8b1b8790350492642bb93ada178a
Tatchagon Koonkoei <o_k_t@hotmail.com>
2018-01-30 23:08:49 +0700

    Fix column name of pets name and pets breed

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -16,8 +16,8 @@ public final class PetContract {
         public static final String TABLE_NAME = "pets";
 
         public static final String _ID = BaseColumns._ID;
-        public static final String COLUMN_PET_NAME = "์name";
-        public static final String COLUMN_PET_BREED = "์breed";
+        public static final String COLUMN_PET_NAME = "name";
+        public static final String COLUMN_PET_BREED = "breed";
         public static final String COLUMN_PET_GENDER = "gender";
         public static final String COLUMN_PET_WEIGHT = "weight";
 
