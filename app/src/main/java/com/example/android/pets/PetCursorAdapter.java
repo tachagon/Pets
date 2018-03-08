4e7a4e16c7b38261e009be95cb7842540079a926
Tatchagon Koonkoei <o_k_t@hotmail.com>
2018-03-08 16:49:54 +0700

    Lesson 4: 17. Unknown Breed

--- app/src/main/java/com/example/android/pets/PetCursorAdapter.java
+++ app/src/main/java/com/example/android/pets/PetCursorAdapter.java
@@ -2,6 +2,7 @@ package com.example.android.pets;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -65,7 +66,13 @@ public class PetCursorAdapter extends CursorAdapter {
         String petName = cursor.getString(nameColumnIndex);
         String petBreed = cursor.getString(breedColumnIndex);
 
-        // Populate fields with extracted properties
+        // If the pet breed is empty string or null, then use some default text
+        // that says "Unknown breed", so the TextView isn't blank.
+        if (TextUtils.isEmpty(petBreed)) {
+            petBreed = context.getString(R.string.unknown_breed);
+        }
+
+        // Update the TextViews with the attributes for the current pet
         nameTextView.setText(petName);
         summaryTextView.setText(petBreed);
     }
